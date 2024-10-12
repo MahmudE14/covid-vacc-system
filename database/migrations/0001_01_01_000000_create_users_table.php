e7d7c879c1cd3cde6f296e8add5805be209177f4
mahmude14 <mahmud.e14@gmail.com>
2024-10-12 19:29:42 +0600

    mod: add index on nid in users table
    Added index on nid in users table to improve performance.

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('nid', 10)->unique();
+            $table->string('nid', 10)->unique()->index('users_nid_unique');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
